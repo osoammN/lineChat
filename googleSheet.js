006a7af3408e6f3a31f6cc855ac105306906222c
osoammN <89015143+osoammN@users.noreply.github.com>
2025-04-28 17:35:17 +0700

    Update googleSheet.js

--- googleSheet.js
+++ googleSheet.js
@@ -1,13 +1,17 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const creds = require('./service-account.json');
 
-const SHEET_ID = '1s6nibdbq4f0xenlctcOKo_w5cb_JH3P4gcjC_Bb3IK4'; // ✅ The long ID from the sheet URL
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+const SHEET_ID = '1s6nibdbq4f0xenlctcOKo_w5cb_JH3P4gcjC_Bb3IK4'; // ❗️Use your actual Google Sheet ID
+
+const creds = {
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+};
 
 async function logToGoogleSheet(question, answer, author) {
   const doc = new GoogleSpreadsheet(SHEET_ID);
 
   await doc.useServiceAccountAuth(creds);
-
   await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0];
 
