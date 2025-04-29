5c765d1cf0e797d6e566fbf7d571a01f0ded2d61
osoammN <89015143+osoammN@users.noreply.github.com>
2025-04-29 20:45:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,25 @@ app.post('/webhook', line.middleware(config), (req, res) => {
       const answer = parts[2]?.trim();
       const type = parts[3]?.trim();
       const author = parts[4]?.trim();
-      if (func === "s" || "S"){
-      const type = parts[3]?.trim();
-        await logToGoogleSheet(question, answer,type, author);
+      if (func === "s" || func === "S") {
+        const type = parts[3]?.trim();
+        await logToGoogleSheet(question, answer, type, author);
         await client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: `📩 Received: "${message}"`,
-      });
+          type: 'text',
+          text: `📩 Received: "${message}"`,
+        });
+      }      
     }
-    }
+      
+      
+      
+
+      // ✅ Reply to user
+    //   await client.replyMessage(event.replyToken, {
+    //     type: 'text',
+    //     text: `📩 Received: "${message}"`,
+    //   });
+    
   }))
   .then(() => res.status(200).end())
   .catch(err => {
